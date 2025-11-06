d4fe8395bc3835d0c3c722baba39dd353156196f
Ann00012 <annamalchevskakotic@gmail.com>
2025-11-06 09:57:36 +0200

    update js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
-import 'modern-normalize/modern-normalize.css';
+import Accordion from 'accordion-js';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import iziToast from 'izitoast';
+import raty from 'raty-js';
