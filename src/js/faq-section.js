df912e7c88f0525579f8ea9ef0ff60367d6bde46
SvitlanaKovshun <svetik_d@ukr.net>
2025-11-08 16:51:30 +0200

    add faq-section first version

--- src/js/faq-section.js
+++ src/js/faq-section.js
@@ -1,4 +1,12 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.accordion-container', {});
+new Accordion('.accordion-container', {
+  duration: 500,
+  ariaEnabled: true,
+  collapse: true,
+  elementClass: 'ac',
+  beforeOpen() {},
+});
+
+// import './js/faq-section';
